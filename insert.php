f08d1a92d2ceabb53d8563d11f66f73037ca7b29
Dimpi0309 <168347395+Dimpi0309@users.noreply.github.com>
2024-07-22 11:47:14 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel = "stylesheet" href = "styleinsert.css">
+</head>
+<body>
+<header>
+<div class="title">
+<h1><u>CITY HOSPITAL</u></h1>
+</div>
+<div class="button">
+	<a href="insert_patient.php" class ="btn">INSERT INTO PATIENT TABLE</a>&nbsp&nbsp&nbsp
+	<a href="insert_doctor.php" class ="btn">INSERT INTO DOCTOR TABLE</a>
+</div>
+</header>
+</body>
+</html>
