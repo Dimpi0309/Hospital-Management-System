e36c324b71eaeb957eadcae81d69a2177b90e679
Dimpi0309 <168347395+Dimpi0309@users.noreply.github.com>
2024-07-22 11:43:31 +0530

    Add files via upload

---
+++ hospital.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+<title>hospital management system</title>  
+ <link rel = "stylesheet" href = "stylehospital.css"> 
+  
+</head>
+
+<body>
+<header>
+
+<div class="title">
+<h1><u>CITY HOSPITAL</u></h1>
+</div>
+
+<div class="button">
+<a href="admin.php" class="btn">ADMIN LOGIN</a>&nbsp&nbsp&nbsp
+<a href="bedbooking.php" class="btn">CORONA BED BOOKING</a>
+</div>
+
+</header>
+</body>
+</html>
