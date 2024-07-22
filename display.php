c9fd6d001ccee3651eda4ce0ffebc4803d43b761
Dimpi0309 <168347395+Dimpi0309@users.noreply.github.com>
2024-07-22 11:44:57 +0530

    Add files via upload

---
+++ display.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<link rel = "stylesheet" href = "styledisplay.css">
+</head>
+<body>
+<header>
+	<div class="title">
+		<h1><u>CITY HOSPITAL</u></h1>
+	</div>
+	<div class="button">
+	&nbsp&nbsp&nbsp&nbsp<a href="display_patient.php" class="btn">PATIENT</a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="display_doctor.php" class="btn">DOCTOR</a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="display_dept.php" class="btn">DEPARTMENT </a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="display_icu.php" class="btn">ICU </a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="display_diagonistic.php" class="btn">DIAGONISIC </a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="join-doc-pat.php" class="btn">PATIENT,ADDMISSION AND DOCTOR</a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="join-pat-bill.php" class="btn">PATIENT AND BILL </a><br><br><br>&nbsp&nbsp&nbsp
+	<a href="join-staff-build.php" class="btn">STAFF AND BUILDING</a>
+	</div>
+	</header>
+</body>
+</html>
